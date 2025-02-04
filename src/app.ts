db1ffcf3c34b9d80cdcad35d974a70748ecbb44d
hammadsadi <hammad.sadi@yahoo.com>
2025-02-04 11:21:41 +0600

    Update Return URL

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ import cookieParser from 'cookie-parser';
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://bicycle-haven.vercel.app'],
+    credentials: true,
+  }),
+);
 // application routes
 app.use('/api/v1', router);
 
